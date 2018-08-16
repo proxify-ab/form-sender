5e34c469fe98174b5cc5626b29688a9c98066a44
Maksym Khartanovych <m.khartanovych@gmail.com>
2018-08-16 22:01:36 +0300

    Configure frontend part

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Mail\Image;
+use App\Mail\ImageMail;
 use Illuminate\Support\Facades\Mail;
 
 /**
@@ -16,6 +16,6 @@ class MailController extends Controller
 {
     public function index()
     {
-        Mail::send(new Image());
+        Mail::send(new ImageMail());
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js/form-sender.js')
+    .sass('resources/assets/sass/app.scss', 'public/css/form-sender.css');
